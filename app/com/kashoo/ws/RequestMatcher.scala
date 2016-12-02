5c8241e3574d26c47ed738e4ba0bbe583c617cf9
Dave Kichler <dave.kichler@kashoo.com>
2016-12-02 14:57:27 -0800

    RequestMatcher first pass of testing green

--- app/com/kashoo/ws/RequestMatcher.scala
+++ app/com/kashoo/ws/RequestMatcher.scala
@@ -7,19 +7,12 @@ import play.api.Configuration
 /**
   * Defines a matcher for comparing against requests.
   *
-  * @param host
-  * @param port
-  * @param path
+  * @param host host to match against - required
+  * @param port optional port to match against
+  * @param path optional path to match against.  In the case that a request matches two matchers, the one with the longest path
+  *             wins
   */
-case class RequestMatcher(rate: Rate, host: String, port: Option[Int] = None, path: Option[String] = None) {
-
-  def check(uri: URI) = {
-    def hostMatch = uri.getHost.equals(host)
-    def portMatch = uri.getPort.equals(port.getOrElse(uri.getPort))
-    def pathMatch = uri.getPath.equals(path.getOrElse(uri.getPath))
-    hostMatch && portMatch && pathMatch
-  }
-}
+case class RequestMatcher(rate: Rate, host: String, port: Option[Int] = None, path: Option[String] = None)
 
 object RequestMatcher {
   def apply(config: Configuration): RequestMatcher = {
@@ -30,30 +23,39 @@ object RequestMatcher {
     RequestMatcher(Rate(config, rate), host, port, path)
   }
 
-  def matchRequest(uri: URI, matchers: Set[RequestMatcher]): Option[RequestMatcher] = {
-    specificityMatchers.find {
-      case matcher if matcher(UriWrapper(uri), matchers).isDefined => true
-      case _ => false
-    } flatMap { matcher =>
-      specificityMatchers
-      matcher(UriWrapper(uri), matchers)
+  /**
+    * Matches the most specific matcher given a set of RequestMatchers and a URI
+    * @param uri to match against
+    * @param matchers to match with
+    * @return an optional request matcher
+    */
+  def matchRequest(uri: URI, matchers: Set[RequestMatcher]): Option[RequestMatcher] =
+    specificityMatchers.collectFirst {
+      case matcher if matcher(UriWrapper(uri), matchers).isDefined => matcher(UriWrapper(uri), matchers).get
     }
-  }
 
   private val specificityMatchers: Seq[Matcher] = Seq(matchAll, matchHostAndPort, matchHost)
 
   private type Matcher = (UriWrapper, Set[RequestMatcher]) => Option[RequestMatcher]
 
-  private def matchAll: Matcher = (uri, matchers) => matchers find { rm =>
-    rm.host == uri.getHost && rm.port == uri.getPort && (rm.path == uri.getPath || pathsMatch(uri.getPath, rm.path))
+  /**
+    * Most specific matcher - matches on all fields.  When more than a single matcher are matched, the one with the longer path wins (more specific)
+    */
+  private def matchAll: Matcher = (uri, matchers) => matchers.collect {
+    case rm if rm.host == uri.getHost && rm.port == uri.getPort && (rm.path == uri.getPath || pathsMatch(uri.getPath, rm.path)) =>
+      rm
+  } match {
+    case matches if matches.isEmpty => None
+    case matches => Some(matches.maxBy(_.path.get.length))
   }
 
   private def matchHostAndPort: Matcher = (uri, matchers) => matchers find { rm =>
     rm.host == uri.getHost && rm.port == uri.getPort
   }
 
-  private def matchHost: Matcher = (uri, matchers) => matchers find { rm =>
-    rm.host == uri.getHost
+  private def matchHost: Matcher = (uri, matchers) => matchers find {
+    case RequestMatcher(_, host, None, None) if host == uri.getHost => true
+    case _ => false
   }
 
   private def pathsMatch(uriPath: Option[String], matcherPath: Option[String]): Boolean = (uriPath, matcherPath) match {
--- test/com/kashoo/ws/RequestMatcherSpec.scala
+++ test/com/kashoo/ws/RequestMatcherSpec.scala
@@ -14,21 +14,35 @@ class RequestMatcherSpec extends FlatSpec with Matchers {
 
     val mostSpecific = RequestMatcher(basicRate, "example.com", Some(9001), Some("some/path/with/several/segments"))
     val nextSpecific = RequestMatcher(basicRate, "example.com", Some(9001), Some("some/path"))
+    val specificWithoutPort = RequestMatcher(basicRate, "example.com", None, Some("some/path/with/several/segments"))
+    val shorterPathSpecificWithoutPort = RequestMatcher(basicRate, "example.com", None, Some("some/path"))
     val lessSpecific = RequestMatcher(basicRate, "example.com", Some(9001))
     val leastSpecific = RequestMatcher(basicRate, "example.com")
 
-    val matchers = Set(leastSpecific, lessSpecific, nextSpecific, mostSpecific)
+    val matchers = Set(leastSpecific, lessSpecific, nextSpecific, mostSpecific, specificWithoutPort, shorterPathSpecificWithoutPort)
 
     def assertMatch(testUri: URI, expectedMatcher: RequestMatcher) =
       RequestMatcher.matchRequest(testUri, matchers) shouldBe Some(expectedMatcher)
   }
 
-  "RequestMatcher" should "match the most specific matcher for a URI matching all path segments" in new TestScope {
+  "RequestMatcher" should "match the most specific matcher for a URI with matching host, port, and all path segments" in new TestScope {
     assertMatch(new URI("http://example.com:9001/some/path/with/several/segments/endpoint"), mostSpecific)
   }
 
-  it should "match the next most specific matcher for a URI matching all path segments of a less specific matcher" in new TestScope {
+  it should "match the most specific matcher for a URI with matching host, port, and all path segments with a trailing slash" in new TestScope {
+    assertMatch(new URI("http://example.com:9001/some/path/with/several/segments/endpoint/"), mostSpecific)
+  }
+
+  it should "match the most specific matcher with a shorter path for a URI with matching host, port and shorter path segment" in new TestScope {
     assertMatch(new URI("http://example.com:9001/some/path/with/different/segments"), nextSpecific)
+  }
+
+  it should "match a specific matcher without a port for a URI with matching host and all path segments" in new TestScope {
+    assertMatch(new URI("http://example.com/some/path/with/several/segments/endpoint/"), specificWithoutPort)
+  }
+
+  it should "match a specific matcher without a port for a URI with matching host and a shorter path segment" in new TestScope {
+    assertMatch(new URI("http://example.com/some/path/with/several/slugs"), shorterPathSpecificWithoutPort)
   }
 
   it should "match the less specific matcher for a URI matching only host and port" in new TestScope {
